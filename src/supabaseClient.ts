c831779f0a4d3efabad557b6223227f1b1418bd7
henningholm <henningholm90@gmail.com>
2025-05-26 18:08:22 +0200

    hardcode env variables

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://wyxunehbvnomqpmxkiuz.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5eHVuZWhidm5vbXFwbXhraXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNjIwNTIsImV4cCI6MjA2MzgzODA1Mn0.WkHqBw4NypN-B80S4ouEbqb78ERRsU5Tr37Uddl4TLw"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
